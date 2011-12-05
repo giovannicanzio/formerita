dfc6892d6ffa3e45dcf458e3f035d8dd11b2b625
Giovanni Canzio <giovanni.canzio@gmail.com>
2011-12-05 12:38:28 +0100

    First basic files creation
    just index and common css file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>formerita | a form framework</title>
+        <link type="text/css" href="css/common.css" />
+    </head>
+    <body>
+    
+    </body>
+
+</html> 
