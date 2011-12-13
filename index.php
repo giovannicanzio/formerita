18c6da48973ea3cb5d036e59b582c721d44eb222
Giovanni Canzio <giovanni.canzio@gmail.com>
2011-12-14 00:36:53 +0100

    form created
    styles defined, included 2 drupal themes (Danland and Acquia Marina),
    the first is clean and ready to include.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <title>formerita | a form framework</title>
         <link type="text/css" href="css/common.css" rel="stylesheet" />
     </head>
@@ -10,41 +11,62 @@
         <!-- I build the main form(s) wrapper. I guess it'll have a 100% width, for now. -->
         <div id="formsWrapper" class="fullWidth">
         
-            <form name="exampleForm1" method="POST" action="#" id="exampleForm1_id">
+            <form name="exampleForm1" method="POST" action="#" id="exampleForm1_id" class="fullWidth">
                 
-                <fieldset>
+                <fieldset class="fullWidth">
                     
-                    <legend>Example Form 1 Legend 1</legend>
+                    <legend class="fullWidth">legend</legend>
                     
-                    <label for="exampleForm1_fieldset1_item1_text_id">label</label>
-                    <input id="exampleForm1_fieldset1_item1_text_id" type="text" name="exampleForm1_fieldset1_item1_text_name" value="value" />
+                    <span class="fullWidth">
+                        <label for="exampleForm1_fieldset1_item1_text_id" class="fullWidth ">label</label>
+                        <input id="exampleForm1_fieldset1_item1_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item1_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset1_item2_text_id">label</label>
-                    <input id="exampleForm1_fieldset1_item2_text_id" type="text" name="exampleForm1_fieldset1_item2_text_name" value="value" />
+                    <span class="halfWidth">
+                        <label for="exampleForm1_fieldset1_item2_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item2_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item2_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset1_item3_text_id">label</label>
-                    <input id="exampleForm1_fieldset1_item3_text_id" type="text" name="exampleForm1_fieldset1_item3_text_name" value="value" />
+                    <span class="halfWidth">
+                        <label for="exampleForm1_fieldset1_item3_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item3_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item3_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset1_item4_text_id">label</label>
-                    <input id="exampleForm1_fieldset1_item4_text_id" type="text" name="exampleForm1_fieldset1_item4_text_name" value="value" />
+                    <span class="thirdWidth">
+                        <label for="exampleForm1_fieldset1_item4_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item4_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item4_text_name" value="value" />
+                    </span>
                     
-                </fieldset>
-                
-                <fieldset>
+                    <span class="thirdWidth">
+                        <label for="exampleForm1_fieldset1_item5_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item5_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item5_text_name" value="value" />
+                    </span>
                     
-                    <legend>Example Form 1 Legend 2</legend>
+                    <span class="thirdWidth">
+                        <label for="exampleForm1_fieldset1_item6_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item6_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item6_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset2_item1_text_id">label</label>
-                    <input id="exampleForm1_fieldset2_item1_text_id" type="text" name="exampleForm1_fieldset2_item1_text_name" value="value" />
+                    <span class="quarterWidth">
+                        <label for="exampleForm1_fieldset1_item7_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item7_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item7_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset2_item2_text_id">label</label>
-                    <input id="exampleForm1_fieldset2_item2_text_id" type="text" name="exampleForm1_fieldset2_item2_text_name" value="value" />
+                    <span class="quarterWidth">
+                        <label for="exampleForm1_fieldset1_item8_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item8_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item8_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset2_item3_text_id">label</label>
-                    <input id="exampleForm1_fieldset2_item3_text_id" type="text" name="exampleForm1_fieldset2_item3_text_name" value="value" />
+                    <span class="quarterWidth">
+                        <label for="exampleForm1_fieldset1_item9_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item9_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item9_text_name" value="value" />
+                    </span>
                     
-                    <label for="exampleForm1_fieldset2_item4_text_id">label</label>
-                    <input id="exampleForm1_fieldset2_item4_text_id" type="text" name="exampleForm1_fieldset2_item4_text_name" value="value" />
+                    <span class="quarterWidth">
+                        <label for="exampleForm1_fieldset1_item10_text_id" class="fullWidth">label</label>
+                        <input id="exampleForm1_fieldset1_item10_text_id" class="fullWidth" type="text" name="exampleForm1_fieldset1_item10_text_name" value="value" />
+                    </span>
+                    
                     
                 </fieldset>
                 
