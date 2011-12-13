86010f5c51c1d8967aa0aeb535ac7be4bb9187bb
Giovanni Canzio <giovanni.canzio@gmail.com>
2011-12-13 01:30:07 +0100

    First elements and styles

--- TODO.markdown
+++ TODO.markdown
@@ -1,2 +1,6 @@
 # TODO 
-* ...
+* build a complete form example
+* test it in various html templates
+* code comments revision
+* build a layout customizer (jQuery) and try to optimize via media-queries
+* publish it
--- index.php
+++ index.php
@@ -2,9 +2,55 @@
 <html>
     <head>
         <title>formerita | a form framework</title>
-        <link type="text/css" href="css/common.css" />
+        <link type="text/css" href="css/common.css" rel="stylesheet" />
     </head>
+    
     <body>
+        
+        <!-- I build the main form(s) wrapper. I guess it'll have a 100% width, for now. -->
+        <div id="formsWrapper" class="fullWidth">
+        
+            <form name="exampleForm1" method="POST" action="#" id="exampleForm1_id">
+                
+                <fieldset>
+                    
+                    <legend>Example Form 1 Legend 1</legend>
+                    
+                    <label for="exampleForm1_fieldset1_item1_text_id">label</label>
+                    <input id="exampleForm1_fieldset1_item1_text_id" type="text" name="exampleForm1_fieldset1_item1_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset1_item2_text_id">label</label>
+                    <input id="exampleForm1_fieldset1_item2_text_id" type="text" name="exampleForm1_fieldset1_item2_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset1_item3_text_id">label</label>
+                    <input id="exampleForm1_fieldset1_item3_text_id" type="text" name="exampleForm1_fieldset1_item3_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset1_item4_text_id">label</label>
+                    <input id="exampleForm1_fieldset1_item4_text_id" type="text" name="exampleForm1_fieldset1_item4_text_name" value="value" />
+                    
+                </fieldset>
+                
+                <fieldset>
+                    
+                    <legend>Example Form 1 Legend 2</legend>
+                    
+                    <label for="exampleForm1_fieldset2_item1_text_id">label</label>
+                    <input id="exampleForm1_fieldset2_item1_text_id" type="text" name="exampleForm1_fieldset2_item1_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset2_item2_text_id">label</label>
+                    <input id="exampleForm1_fieldset2_item2_text_id" type="text" name="exampleForm1_fieldset2_item2_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset2_item3_text_id">label</label>
+                    <input id="exampleForm1_fieldset2_item3_text_id" type="text" name="exampleForm1_fieldset2_item3_text_name" value="value" />
+                    
+                    <label for="exampleForm1_fieldset2_item4_text_id">label</label>
+                    <input id="exampleForm1_fieldset2_item4_text_id" type="text" name="exampleForm1_fieldset2_item4_text_name" value="value" />
+                    
+                </fieldset>
+                
+            </form>
+            
+        </div>
     
     </body>
 
